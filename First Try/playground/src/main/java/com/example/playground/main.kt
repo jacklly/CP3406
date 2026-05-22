0093f2d6a3d36c63c094589b77fdaebb9b6c3d60
jacklly <jack.kelly1@my.jcu.edu.au>
2026-05-22 12:40:37 +1000

    proper setup!

---
+++ First Try/playground/src/main/java/com/example/playground/main.kt
@@ -0,0 +1,18 @@
+package com.example.playground
+
+fun main() {
+    val recipe = Recipe() // Create an instance of Recipe class
+    recipe.addIngredient("Rice") // Call the method with argument “Rice”
+    recipe.addIngredient("Beans") // Call it again, with “Beans”
+    println(recipe.getIngredients()) // Print this method to console
+}
+
+class Recipe {
+    private val ingredients = mutableListOf<String>()
+    fun addIngredient(name: String) {
+        ingredients.add(name)
+    }
+    fun getIngredients(): List<String> {
+        return ingredients
+    }
+}
